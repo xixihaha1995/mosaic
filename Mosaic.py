1812ece34942a5c4f79622e500ca6ec2d5fd8ce5
xixihaha1995 <43649779+xixihaha1995@users.noreply.github.com>
2019-04-16 19:35:48 -0400

    Update Mosaic.py

--- Mosaic.py
+++ Mosaic.py
@@ -2,39 +2,19 @@
 # coding: utf-8
 
 # In[1]:
-
-
 import numpy as np
 from PIL import Image
 import os
-
-
-# In[2]:
-
-
-import cv2
-
-
-# In[2]:
-
 
 def unpickle(file):
     import pickle
     with open(file, 'rb')as fo:
         dict = pickle.load(fo, encoding = 'latin1')
     return dict
-
-
 # In[3]:
-
-
 #print(dict.keys())
 # dict_keys(['filenames','batch_label', 'fine_labels', 'coarse_labels', 'data'])
-
-
 # In[4]:
-
-
 def loadCifarBatch(filename, foldername):
     dict = unpickle(filename)
     batch_label = dict['batch_label']
@@ -49,18 +29,10 @@ def loadCifarBatch(filename, foldername):
     elif foldername == "test":
             data = data.reshape(10000, 3, 32, 32)
     return data, batch_label, fine_labels, coarse_labels
-
-
 # In[5]:
-
-
 # name = classname + "_" +str(trainImgNum) + ".png" 
 # saveimgname = os.path.join(foldername,classname, name)
-
-
 # In[6]:
-
-
 def saveImage(filename, foldername):
     imagedata, batch_label, fine_labels, coarse_labels = loadCifarBatch(filename, foldername)
     label2classdict = unpickle('cifar-100-python/meta')
@@ -88,37 +60,23 @@ def saveImage(filename, foldername):
             os.makedirs(savepath)
         img.save(saveimgname, "JPEG")
         trainImgNum += 1
-
-
 # In[7]:
-
-
 def main():
     filename = 'cifar-100-python/train'
     foldername = "train"
     saveImage(filename, foldername)
 if __name__ == "__main__":
     main()
-
-
-
 # In[1]:
-
-
 import cv2
 img = cv2.imread("hh.jpg")
 print(img.shape)
 res = cv2.resize(img, (2400, 1800))
 cv2.imwrite("hh_1.jpg", res)
-
-
 # In[10]:
-
-
 import photomosaic as pm
 
 image = pm.imread("hh_1.jpg")
 pool = pm.make_pool('train/*.jpg', sample_size=5000)
 mosaic = pm.basic_mosaic(image, pool, (480, 360))
 pm.imsave('montage2.jpg', mosaic)
-
